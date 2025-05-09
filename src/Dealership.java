fdf15d099299835bb08b69db5067e67d265a531f
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-09 12:33:29 -0700

    update

--- src/Dealership.java
+++ src/Dealership.java
@@ -1,15 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    //CONSTRUCTOR
+    Dealership(String name, String address, String phone){
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<Vehicle>();
+    }
+    public String toString(){
+        return String.format("DEALERSHIP NAME: '%s' #CARS: %d\n", this.name, this.inventory.size());
+    }
+    //METHODS
+    public List<Vehicle> getVehiclesByPrice(double min, double max){
+        return inventory;
+    }
+    public List<Vehicle> getVehiclesByPriceMakeModel(String make, String model){
+        return inventory;
+    }
+    public List<Vehicle> getVehiclesByYear(int min, int max){
+        return inventory;
+    }
+    public List<Vehicle> getVehiclesByMileage(int min, int max){
+        return inventory;
+    }
+    public List<Vehicle> getVehiclesByType(String Type){
+        return inventory;
+    }
+    public List<Vehicle> getAllVehicles(){
+        return inventory;
+    }
+
+    Vehicle addVehicle(Vehicle v){
+        inventory.add(v);
+        return v;
+    }
+    void removeVehicle(Vehicle v){
+        inventory.remove(v);
+    }
+
+    public static void main(String[] args) {
+        var d = new Dealership("XYZ MOTORS", "", "");
+
+     //ADD TO INVENTORY
+        d.addVehicle(new Vehicle(123,
+             1999,
+             "MAZDA",
+             "MX-5",
+             "ROADSTER",
+             "WHITE",
+             128000,
+             5000));
+        d.addVehicle(new Vehicle(456,
+             2004,
+             "Toyota",
+             "Camry",
+             "Sedan",
+             "Silver",
+             156000,
+             3000));
+
+        System.out.println(d); //show dealership
+
+        for(Vehicle v : d.getAllVehicles()){
+            System.out.println(v);
         }
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+class Vehicle {
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
 
-public class Vehicle {
+    Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer,double price){
+         this.vin = vin;
+         this.year = year;
+         this.make = make;
+         this.model = model;
+         this.vehicleType = vehicleType;
+         this.color = color;
+         this.price = price;
+         this.odometer = odometer;
+    }
+    Vehicle(int vin){    // OVERLOADED CONSTRUCTOR
+        this.vin = vin;  // assign value from parameter to property, without this line it will receive arguments but not set them.
+    }
+    public String toString(){
+        return String.format("VIN: '%d'\nYEAR: '%d'\nMAKE: '%s'\nMODEL: '%s'\n",
+                this.vin, this.year, this.make, this.model);
+    }
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle(123, 1999, "MAZDA", "MX-5", "ROADSTER", "WHITE", 128000, 5000);
+        Vehicle v2 = new Vehicle(456, 2004, "Toyota", "Camry", "Sedan", "Silver", 156000, 3000);
+        Vehicle v3 = new Vehicle(789);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.printf("VIN: '%d', ", v3.vin);
+
+    }
 }
