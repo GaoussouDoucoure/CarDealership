fe768405ddb852f84eeee26ef23f163db9539649
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-16 23:06:19 -0700

    Added more getters and modified previous getters

--- src/LeaseContract.java
+++ src/LeaseContract.java
@@ -1,8 +1,8 @@
 import java.util.Date;
 
 public class LeaseContract extends BusinessContract {
-    private double expectedEndingValue;
-    private double leaseFee;
+    protected double expectedEndingValue;
+    protected double leaseFee;
 
     LeaseContract(
             /* ******** BASE ********* */
@@ -14,12 +14,42 @@ public class LeaseContract extends BusinessContract {
             double totalPrice,
             /* ******** NEW ********* */
             double expectedEndingValue,
-            double leaseFee
+            double leaseFee,
+            double monthlyPayment
     ) {
-        super(vehicle, date, customerName, customerEmail, isSold, totalPrice);
+        super(vehicle, date, customerName, customerEmail, true);
+        this.totalPrice = totalPrice;
         this.expectedEndingValue = expectedEndingValue;
         this.leaseFee = leaseFee;
+        this.monthlyPayment = monthlyPayment;
     }
+    LeaseContract(
+            /* ******** BASE ********* */
+            Vehicle vehicle,
+            Date date,
+            String customerName,
+            String customerEmail
+    ) {
+        super(vehicle, date, customerName, customerEmail, true);
+        this.setTotalPrice( this.getTotalPrice());
+        double originalPrice = vehicle.getPrice();
+        this.expectedEndingValue = originalPrice / 2;
+        this.leaseFee = originalPrice * 0.07;
+        this.monthlyPayment = getMonthlyPayment();
+    }
+    public double getExpectedEndingValue(){
+        return expectedEndingValue;
+    }
+    public double getLeaseFee(){
+        return leaseFee;
+    }
+    /*
+    A LeaseContract will include the following additional information:
+• Expected Ending Value (50% of the original price)
+• Lease Fee (7% of the original price)
+• Monthly payment based on
+• All leases are financed at 4.0% for 36 months
+     */
 
     public String toString() {
         return super.toString() + String.format("""
@@ -43,10 +73,22 @@ getTotalPrice() and getMonthlyPayment() that will return computed
 values based on the rules above
  */
     double getTotalPrice() {
-        return 0;
+        double price = vehicle.getPrice();
+
+        //Expected Ending Value (50% of the original price)
+        double expectedEndingValue =  price / 2;
+
+        //• Lease Fee (7% of the original price)
+        double leaseFee = 0.07 * price;
+
+        return expectedEndingValue + leaseFee;
     }
 
     double getMonthlyPayment() {
-        return 0;
+        //• Monthly payment based on
+        //• All leases are financed at 4.0% for 36 months
+        int payments = 36;
+        double rate = 0.04 / 12;
+        return totalPrice * (rate / (1 - Math.pow(1 + rate, -payments)));
     }
 }
